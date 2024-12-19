88b2874011a814b8861caf4f7b63242d36c05bcd
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-19 17:16:22 +0700

    file config

--- src/main/java/com/lightcodese/dsa/controller/About.java
+++ src/main/java/com/lightcodese/dsa/controller/About.java
@@ -1,12 +1,18 @@
 package com.lightcodese.dsa.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class About {
+    @Value("${player.name}")
+    private String PlayerName;
+    @Value("${player.age}")
+    private int PlayerAge;
+
     @GetMapping("/about")
     public String about() {
-        return "Hello World";
+        return PlayerName + " " + PlayerAge;
     }
 }
